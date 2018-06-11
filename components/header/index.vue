9c4fb0794c325526a874d22ad557b97ecc8ee2a3
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2018-06-11 17:50:59 +0800

    udpate

---
+++ components/header/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="page-header">
+    <div class="logo"></div>
+    <nav> daohang </nav>
+  </section>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+   .page-header {
+    background: url("~/assets/images/banner.png") no-repeat;
+    width : 100%;
+  } 
+
+  .page-header .logo {
+      width : 380px;
+      height: 120px;
+      background: url("~/assets/images/logo.png") no-repeat;
+  }
+  
+  .page-header  nav {
+  }
+</style>
+
+
