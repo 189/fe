14f7b9df1e36a592ca7b41c077bad0d7feb3d47a
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2018-06-11 18:35:59 +0800

    title

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,7 +1,19 @@
 <template>
   <section class="page-header">
-    <div class="logo"></div>
-    <nav> daohang </nav>
+    <div class="banner"></div>
+    <div class="nav-box">
+      <div class="grid">
+        <div class="logo"></div>
+        <ul class="nav">
+          <li>首页</li>
+          <li>米其林预定</li>
+          <li>自由行推荐</li>
+          <li>包车/送机</li>
+          <li>目的地</li>
+          <li>了解日本</li>
+        </ul>
+      </div>
+    </div>
   </section>
 </template>
 <script>
@@ -12,18 +24,40 @@ export default {
 
 <style>
   .page-header {
-    background: url("~/assets/images/banner.png") no-repeat;
     width : 100%;
   } 
-
+  .page-header .banner {
+    background: url("~/assets/images/banner.png") no-repeat;
+    height : 74px;
+  }
+  .page-header .nav-box { 
+    background-color : #dd1415; 
+  }
+  .page-header .grid {
+    padding : 0 100px;
+    position: relative;
+  }
   .page-header .logo {
     width : 380px;
     height: 120px;
+    position: absolute;
+    left : 100px;
+    bottom : -35px;
     background: url("~/assets/images/logo.png") no-repeat;
   }
   
-  .page-header  nav {
-    
+  .page-header  .nav {
+    margin-left : 390px;
+    height : 58px;
+    line-height: 58px;
+    font-size : 16px;
+    text-align: right;
+    color : #fff;
+  }
+
+  .page-header  .nav li {
+    display: inline-block;
+    margin: 0 20px;
   }
 </style>
 
