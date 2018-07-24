247ddcb377a6ecadc3e6d9408ed6008256113db8
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2018-07-24 20:10:20 +0800

    update

---
+++ components/loginBar.vue
@@ -0,0 +1,23 @@
+<style>
+
+</style>
+
+<template>
+  <div>
+    <a href="javascript:;">登录</a>
+    <span>|</span>
+    <a href="javascript:;">注册</a>
+  </div>
+</template>
+<script>
+export default {
+  name : 'login-bar',
+  data(){
+    return {
+
+    };
+  }
+}
+</script>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,25 @@
 <template>
   <section class="container">
     <page-header />
+    <div>
+      <login-bar></login-bar>
+    </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue';
 import PageHeader from '~/components/header';
+import LoginBar from '~/components/loginBar.vue';
+import { Row, Col } from 'iview';
+
 export default {
   components: {
     AppLogo,
-    PageHeader
+    PageHeader,
+    LoginBar,
+    [Row.name] : Row,
+    [Col.name] : Col
   },
   data(){
     return {
